a46de8dc35017a3f22f0dcb3907b4b09e9dd882e
vibecode <rumashkin@gmail.com>
2019-03-14 22:10:39 +0100

    add prettier config

--- src/auth/actions.js
+++ src/auth/actions.js
@@ -1,5 +1,4 @@
-import firebase from 'firebase/app';
-
+import firebase from 'firebase/app'
 
 export const authActions = {
   SIGN_IN: 'SIGN_IN',
@@ -10,33 +9,29 @@ export const authActions = {
   SIGN_OUT_FAILED: 'SIGN_OUT_FAILED',
   SIGN_OUT_FULFILLED: 'SIGN_OUT_FULFILLED',
 
-
   signIn: authProvider => ({
     type: authActions.SIGN_IN,
-    payload: {authProvider}
+    payload: { authProvider }
   }),
 
   signInFailed: error => ({
     type: authActions.SIGN_IN_FAILED,
-    payload: {error}
+    payload: { error }
   }),
 
   signInFulfilled: authUser => ({
     type: authActions.SIGN_IN_FULFILLED,
-    payload: {authUser}
+    payload: { authUser }
   }),
 
-  signInWithGithub: () => authActions.signIn(
-    new firebase.auth.GithubAuthProvider()
-  ),
+  signInWithGithub: () =>
+    authActions.signIn(new firebase.auth.GithubAuthProvider()),
 
-  signInWithGoogle: () => authActions.signIn(
-    new firebase.auth.GoogleAuthProvider()
-  ),
+  signInWithGoogle: () =>
+    authActions.signIn(new firebase.auth.GoogleAuthProvider()),
 
-  signInWithTwitter: () => authActions.signIn(
-    new firebase.auth.TwitterAuthProvider()
-  ),
+  signInWithTwitter: () =>
+    authActions.signIn(new firebase.auth.TwitterAuthProvider()),
 
   signOut: () => ({
     type: authActions.SIGN_OUT
@@ -44,10 +39,10 @@ export const authActions = {
 
   signOutFailed: error => ({
     type: authActions.SIGN_OUT_FAILED,
-    payload: {error}
+    payload: { error }
   }),
 
   signOutFulfilled: () => ({
     type: authActions.SIGN_OUT_FULFILLED
   })
-};
+}
